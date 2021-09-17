0a4bcdc7d6e69ac901084491b618f38f467978aa
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2021-09-17 23:03:22 +0200

    region typography

--- src/components/RegionPicker.tsx
+++ src/components/RegionPicker.tsx
@@ -1,15 +1,20 @@
-import { MenuItem, Select } from "@mui/material"
+import { MenuItem, Select, Typography } from "@mui/material"
 import { Regions } from "../models/Regions"
 export const RegionPicker = () => {
-    return <Select
-        variant="outlined"
-        autoWidth
-        defaultValue={0}
-    >
-        {
-            Regions.map((region) => {
-                return <MenuItem value={region.id}>{region.name}</MenuItem>
-            })
-        }
-    </Select>
+    return <>
+        <Typography>
+            Region
+        </Typography>
+        <Select
+            variant="outlined"
+            autoWidth
+            defaultValue={0}
+        >
+            {
+                Regions.map((region) => {
+                    return <MenuItem value={region.id}>{region.name}</MenuItem>
+                })
+            }
+        </Select>
+    </>
 }
