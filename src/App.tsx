16535f1aff9d844cab8ce796ce9789beacbbd2fa
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2021-09-21 20:35:00 +0200

    styling v2

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ const App = () => {
   return (
     <RecoilRoot>
       <Grid container alignItems='center' width="100%" height="100%" padding="3em" justifyContent="center">
-        <Grid container item component={Paper} direction='column' spacing={2} minWidth="560px" width="80%" padding="2em" alignItems='center' justifyContent=" center" >
+        <Grid container item component={Paper} direction='column' spacing={2} minWidth="560px" width="80%" padding="2em" alignItems='center' justifyContent="center" >
           <Grid container item>
             <RegionPicker />
           </Grid>
           <Grid container item xs={12}>
             <NewValueButton />
           </Grid>
-          <Grid container item>
+          <Grid container item justifyContent="center">
             <ProductionsTable />
           </Grid>
           <Grid container item>
--- src/components/ProductionsTable.tsx
+++ src/components/ProductionsTable.tsx
@@ -7,108 +7,106 @@ import { Summary } from "./Summary"
 export function ProductionsTable() {
     const [productions, setProductions] = useRecoilState(productionsState)
     const region = useRecoilValue(regionState)
-    return <div>
-        <TableContainer>
-            <Table aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            <Typography variant="h6">
-                                Uprawa
-                            </Typography>
-                        </TableCell>
-                        <TableCell>
-                            <Typography variant="h6">
-                                Wartość jednostkowa (na sztuke/hektar) [€]
-                            </Typography>
-                        </TableCell>
-                        <TableCell>
-                            <Typography variant="h6">
-                                Ilość
-                            </Typography>
-                        </TableCell>
-                        <TableCell>
-                            <Typography variant="h6">
-                                Wartość [€]
-                            </Typography>
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        productions.map((production, i) => {
-                            return <TableRow>
-                                <TableCell>
-                                    <Autocomplete
-                                        id={`$productions-autocomplete-{i}`}
-                                        options={Converters}
+    return <TableContainer>
+        <Table aria-label="customized table">
+            <TableHead>
+                <TableRow>
+                    <TableCell>
+                        <Typography variant="h6">
+                            Uprawa
+                        </Typography>
+                    </TableCell>
+                    <TableCell>
+                        <Typography variant="h6">
+                            Wartość jednostkowa (na sztuke/hektar) [€]
+                        </Typography>
+                    </TableCell>
+                    <TableCell>
+                        <Typography variant="h6">
+                            Ilość
+                        </Typography>
+                    </TableCell>
+                    <TableCell>
+                        <Typography variant="h6">
+                            Wartość [€]
+                        </Typography>
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    productions.map((production, i) => {
+                        return <TableRow>
+                            <TableCell>
+                                <Autocomplete
+                                    id={`$productions-autocomplete-{i}`}
+                                    options={Converters}
 
-                                        getOptionLabel={(opt) => opt.name}
-                                        onChange={(e, conv) => {
-                                            console.log(e.type, conv)
-                                            setProductions(productions.slice(0, i)
-                                                .concat([{
-                                                    converter: (conv !== null) ? conv : undefined,
-                                                    amount: production?.amount,
-                                                }
-                                                ])
-                                                .concat(productions.slice(i + 1, productions.length)))
-                                        }}
-                                        value={production.converter}
-                                        renderInput={(params) => {
-                                            return <TextField {...params} label="Produkcja/Uprawa" />
-                                        }}
-                                    />
-                                </TableCell>
-                                <TableCell>
-                                    <Typography>
-                                        {(production.converter != undefined) ?
-                                            production.converter.values[region.group] : ""}
-                                    </Typography>
-                                </TableCell>
-                                <TableCell>
+                                    getOptionLabel={(opt) => opt.name}
+                                    onChange={(e, conv) => {
+                                        console.log(e.type, conv)
+                                        setProductions(productions.slice(0, i)
+                                            .concat([{
+                                                converter: (conv !== null) ? conv : undefined,
+                                                amount: production?.amount,
+                                            }
+                                            ])
+                                            .concat(productions.slice(i + 1, productions.length)))
+                                    }}
+                                    value={production.converter}
+                                    renderInput={(params) => {
+                                        return <TextField {...params} label="Produkcja/Uprawa" />
+                                    }}
+                                />
+                            </TableCell>
+                            <TableCell>
+                                <Typography>
+                                    {(production.converter !== undefined) ?
+                                        production.converter.values[region.group] : ""}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
 
-                                    <TextField
-                                        id="outlined-number"
-                                        label="Ilość"
-                                        type="number"
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                        defaultValue={production.amount}
-                                        onChange={(v) => {
-                                            setProductions(productions.slice(0, i)
-                                                .concat([{
-                                                    converter: production.converter,
-                                                    amount: parseFloat(v.target.value),
-                                                }
-                                                ])
-                                                .concat(productions.slice(i + 1, productions.length)))
-                                        }}
-                                    />
-                                </TableCell>
-                                <TableCell>
-                                    {(production.converter != undefined) ?
-                                        Number(production.converter.values[region.group] * production.amount).toFixed(2) : ""}
-                                </TableCell>
-                            </TableRow>
+                                <TextField
+                                    id="outlined-number"
+                                    label="Ilość"
+                                    type="number"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    defaultValue={production.amount}
+                                    onChange={(v) => {
+                                        setProductions(productions.slice(0, i)
+                                            .concat([{
+                                                converter: production.converter,
+                                                amount: parseFloat(v.target.value),
+                                            }
+                                            ])
+                                            .concat(productions.slice(i + 1, productions.length)))
+                                    }}
+                                />
+                            </TableCell>
+                            <TableCell>
+                                {(production.converter !== undefined) ?
+                                    Number(production.converter.values[region.group] * production.amount).toFixed(2) : ""}
+                            </TableCell>
+                        </TableRow>
 
-                        })
-                    }
-                    <TableRow>
-                        <TableCell>
-                            Suma
-                        </TableCell>
-                        <TableCell>
-                        </TableCell>
-                        <TableCell>
-                        </TableCell>
-                        <TableCell>
-                            <Summary />
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </TableContainer >
-    </div >
+                    })
+                }
+                <TableRow>
+                    <TableCell>
+                        Suma
+                    </TableCell>
+                    <TableCell>
+                    </TableCell>
+                    <TableCell>
+                    </TableCell>
+                    <TableCell>
+                        <Summary />
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </TableContainer >
 }
