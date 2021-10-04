85505177176269cd779c4cd43e96451f51cba58a
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2021-10-05 00:26:32 +0200

    theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,60 @@
-import { Grid, Paper } from '@mui/material';
-import React from 'react';
-import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, Grid, Paper } from "@mui/material";
+import React from "react";
+import { RecoilRoot } from "recoil";
 import { CSVDownload } from "./components/CSVDownload";
 import { Intro } from "./components/Intro";
 import { NewValueButton } from "./components/NewValueButton";
 import { ProductionsTable } from "./components/ProductionsTable";
 import { RegionPicker } from "./components/RegionPicker";
-
+import useToggleColorMode from "./components/ThemeToggle";
 
 const App = () => {
+  const [colorMode, theme, toggleButton] = useToggleColorMode();
   return (
-    <RecoilRoot>
-      <Grid container alignItems='center' width="100%" height="100%" padding="3em" justifyContent="center">
-        <Grid container item component={Paper} direction='column' spacing={2} minWidth="560px" width="80%" padding="2em" alignItems='center' justifyContent="center" >
-          <Grid container item>
-            <Intro />
-          </Grid>
-          <Grid container item>
-            <RegionPicker />
-          </Grid>
-          <Grid container item xs={12}>
-            <NewValueButton />
-          </Grid>
-          <Grid container item justifyContent="center">
-            <ProductionsTable />
-          </Grid>
-          <Grid container item>
-            <CSVDownload />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RecoilRoot>
+        <Grid
+          container
+          alignItems="center"
+          width="100%"
+          height="100%"
+          padding="3em"
+          justifyContent="center"
+        >
+          <Grid
+            container
+            item
+            component={Paper}
+            direction="column"
+            spacing={2}
+            minWidth="560px"
+            width="80%"
+            padding="2em"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid container item>
+              {toggleButton}
+              <Intro />
+            </Grid>
+            <Grid container item>
+              <RegionPicker />
+            </Grid>
+            <Grid container item xs={12}>
+              <NewValueButton />
+            </Grid>
+            <Grid container item justifyContent="center">
+              <ProductionsTable />
+            </Grid>
+            <Grid container item>
+              <CSVDownload />
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </RecoilRoot >
+      </RecoilRoot>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,46 @@
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import IconButton from "@mui/material/IconButton";
+import { createTheme } from "@mui/material/styles";
+import * as React from "react";
+
+export default function useToggleColorMode() {
+  const [mode, setMode] = React.useState<"light" | "dark">("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          background: {
+            default: mode === "light" ? "#FFF" : "#000",
+          },
+          mode: mode,
+        },
+      }),
+    [mode]
+  );
+
+  const toggleButton = (
+    <IconButton
+      sx={{ ml: 1 }}
+      onClick={colorMode.toggleColorMode}
+      color="inherit"
+    >
+      {theme.palette.mode === "dark" ? (
+        <Brightness7Icon />
+      ) : (
+        <Brightness4Icon />
+      )}
+    </IconButton>
+  );
+
+  return [colorMode, theme, toggleButton];
+}
